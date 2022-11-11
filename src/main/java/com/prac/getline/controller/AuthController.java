b2e739374129b3c0259a404c0a1c847e588b7b6c
NewL <taemin1104@naver.com>
2022-11-11 16:56:21 +0900

    feat: thymeleaf 추가 및 api 기본 셋팅

--- src/main/java/com/prac/getline/controller/AuthController.java
+++ src/main/java/com/prac/getline/controller/AuthController.java
@@ -1,4 +1,18 @@
 package com.prac.getline.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AuthController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "auth/login";
+    }
+
+    @GetMapping("/signUp")
+    public String signUp(){
+        return "auth/signUp";
+    }
 }
